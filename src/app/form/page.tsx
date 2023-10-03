303c7b9f4bb59ab5d36eb315de8160d36bcb2ed6
seinokoula <alexandre.charles92@gmail.com>
2023-10-03 18:25:53 +0200

    :construction: storybook and other packages added

---
+++ src/app/form/page.tsx
@@ -0,0 +1,18 @@
+
+
+export default function Wallet() {
+    return (
+        <div>
+            <div className="lg:pl-72">
+                <main>
+                    <div className="px-4 sm:px-6 lg:px-8">
+                        <p>
+                            Wallet
+                        </p>
+                    </div>
+                </main>
+            </div>
+        </div>
+    )
+
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,14 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'main-bg':'#475569',
+      },
+      
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
 export default config
